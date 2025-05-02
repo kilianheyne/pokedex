588fab15bab5b098e4b84d3ba13ee3a2db706b9f
Kilian Heyne <kilianheyne@icloud.com>
2025-05-02 10:50:47 +0200

    first version of a search function to look for specific Pokémon

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let initArray = [];
 let loadOffset = 0;
+let userSearch = [];
 
 function init(){
     fetchData();
@@ -9,14 +10,16 @@ async function fetchData(){
     let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=${loadOffset}`);
     let pokemonAsJSON = await pokemon.json();
     initArray.push(...pokemonAsJSON.results);
+    userSearch = initArray;
     renderCards();
 }
 
 async function renderCards(){
     let contentRef = document.getElementById('main-content');
-    for(i = 0; i < initArray.length; i++ ){
-        let detailResponse = await fetch(initArray[i].url);
+    for(i = 0; i < userSearch.length; i++ ){
+        let detailResponse = await fetch(userSearch[i].url);
         let pokemonDetails = await detailResponse.json()
+        console.log(pokemonDetails);
         contentRef.innerHTML += cardsTemplate(pokemonDetails);
     }
 }
@@ -29,7 +32,7 @@ function cardsTemplate(pokemon){
                     <span></span>
                 </div>
                 <div class="card-image">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/${pokemon.id}.png" alt="">
+                    <img src="${pokemon.sprites.versions['generation-iii']['ruby-sapphire'].front_default}" alt="">
                 </div>
                 <div class="card-types">
                     ${renderTypes(pokemon)}
@@ -50,4 +53,21 @@ function addNewData(){
     loadOffset += 25;
     initArray = [];
     fetchData();
+}
+
+function inputSearch(){
+    let input = document.getElementById('user-input');
+    let inputValue = input.value.toLowerCase();
+    let mainRef = document.getElementById('main-content');
+
+    if (inputValue.length >= 3) {
+        userSearch = initArray.filter(pokemon => pokemon.name.includes(inputValue));
+        mainRef.innerHTML = "";
+        renderCards();
+    } else {
+        userSearch = initArray;
+        mainRef.innerHTML = "";
+        renderCards();
+        document.getElementById('input-area-span').style.flex;
+    }
 }

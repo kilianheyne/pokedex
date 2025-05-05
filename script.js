f794ece90fe7c803b4b4bc858a9178fe6035e7a4
Kilian Heyne <kilianheyne@icloud.com>
2025-05-05 16:49:55 +0200

    building up an overlay with specific information for each individual pokemon

--- script.js
+++ script.js
@@ -17,14 +17,15 @@ async function fetchData(){
 async function renderCards(){
     let contentRef = document.getElementById('main-content');
     for(i = 0; i < userSearch.length; i++ ){
-        let detailResponse = await fetch(userSearch[i].url);
+        let pokeURL = userSearch[i].url;
+        let detailResponse = await fetch(pokeURL);
         let pokemonDetails = await detailResponse.json()
-        contentRef.innerHTML += cardsTemplate(pokemonDetails);
+        contentRef.innerHTML += cardsTemplate(pokemonDetails, pokeURL);
     }
 }
 
-function cardsTemplate(pokemon){
-    return `<div class="single-card">
+function cardsTemplate(pokemon, pokeURL){
+    return `<div class="single-card" onclick="openOverlay('${pokeURL}')">
                 <div class="card-header">
                     <span class="header-number"># ${pokemon.id}</span>
                     <span class="header-name">${pokemon.name.toUpperCase()}</span>
@@ -73,4 +74,101 @@ function updateInputArea(array, add, remove){
     renderCards();
     document.getElementById('input-area-span').classList.add(add);
     document.getElementById('input-area-span').classList.remove(remove);
+}
+
+async function openOverlay(url){
+    let urlResponse = await fetch(url);
+    let urlDetails = await urlResponse.json()
+    let overlayRef = document.getElementById('overlay');
+    let flavorText = await returnFlavorText(urlDetails);
+    overlayRef.innerHTML = "";
+    overlayRef.style.zIndex = "15";
+    overlayRef.classList.remove('hide');
+    overlayRef.classList.add('show');
+    overlayRef.innerHTML += renderOverlayContainer(urlDetails, flavorText);
+}
+
+function closeOverlay(){
+    let overlayRef = document.getElementById('overlay');
+    overlayRef.style.zIndex = "-5";
+    overlayRef.classList.remove('show');
+    overlayRef.classList.add('hide');
+}
+
+function renderOverlayContainer(pokemonDetails, flavorText){
+    return `<div id="overlay-container">
+                <div id="overlay-header">
+                    <span># ${pokemonDetails.id}</span>
+                    <span>${pokemonDetails.name.toUpperCase()}</span>
+                    <button class="close-overlay" onclick="closeOverlay()">X</button>
+                </div>
+                <div id="overlay-pokemon-details">
+                    <div id="pokemon-visuals">
+                        <div><img src="${pokemonDetails.sprites.versions['generation-iii']['ruby-sapphire'].front_default}" alt=""></div>
+                        <div>
+                            ${returnTypesOverlay(pokemonDetails)}
+                        </div>
+                    </div>
+                    <div id="pokemon-info">
+                        <div>
+                            ${flavorText}
+                        </div>
+                        <div class="show" id="info-tabs">
+                            <div>Base Stats</div>
+                            <div>Evolution Chain</div>
+                            <div>General Information</div>
+                        </div>
+                        <div>
+                            <div class="flexCo" id="base-stats">
+                                ${returnPokemonStats(pokemonDetails)}
+                            </div>
+                            <div>
+                                Hier findest Du die verschiedenen Entwicklung mit Level
+
+                            </div>
+                            <div>
+                                Hier findest Du die allgemeinen Informationen zu einem Pokémon
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+function returnTypesOverlay(details){
+    let pokemonTypes = "";
+    for (let type = 0; type < details.types.length; type++){
+        let typeName = details.types[type].type.name;
+        pokemonTypes += `<span class="pokemon-type type-${typeName}">${typeName.toUpperCase()}</span>`;
+    }
+    return pokemonTypes;
+}
+
+async function returnFlavorText(details){
+    let textResponse = await fetch(details.species.url);
+    let textData = await textResponse.json();
+    let englishText = textData.flavor_text_entries.find(entry => entry.language.name === 'en')?.flavor_text;
+    let finalEnglishText = englishText.replace(/[\n\f\r]/g, ' ');
+    return finalEnglishText;
+}
+
+function returnPokemonStats(details){
+    let baseStats = "";
+    for(let i = 0; i < details.stats.length; i++){
+        let statName = details.stats[i].stat.name;
+        let statPercentage = details.stats[i].base_stat / 2;
+        baseStats += `<div class="stat-bar">
+                        <span class="label">${statName.toUpperCase()}</span>
+                        <div class="bar-bg">
+                            <div class="bar-fill" style="width: ${statPercentage}%"></div>
+                        </div>
+                    </div>`
+    }
+    return baseStats;
+}
+
+async function returnEvolutions(){
+    let evoResponse = await fetch(details.species.url);
+    console.log(evoResponse);
+    
 }
